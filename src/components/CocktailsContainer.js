d297c221e100de9e6d7cbe8e47878bc356a1f75e
github username <github email address>
2018-09-04 19:42:11 -0400

    Done.

--- src/components/CocktailsContainer.js
+++ src/components/CocktailsContainer.js
@@ -4,11 +4,56 @@ import CocktailDisplay from './CocktailDisplay'
 import Form from './Form'
 
 class CocktailsContainer extends Component {
+  constructor () {
+    super()
+    this.state = {
+      cocktails: [],
+      ingredients: [],
+      proportions: [],
+      showCocktail: '',
+      showProportions: []
+    }
+  }
+
+  showCocktail = (cocktail) => {
+    this.setState({showCocktail: cocktail})
+    this.findProportions(cocktail.id)
+  }
+
+  findProportions = (id) => {
+    let props = this.state.proportions.filter(prop => prop.cocktail_id === id)
+    this.setState({showProportions: props})
+
+  }
+
+  findIngredient = (id) => {
+    return this.state.ingredients.find(ing => ing.id === id)
+  }
+
+  componentDidMount() {
+    fetch('https://react-boozer-backend.herokuapp.com/api/v1/cocktails')
+      .then(r => r.json())
+      .then(cocktails => this.setState({cocktails}))
+    fetch('https://react-boozer-backend.herokuapp.com/api/v1/ingredients')
+      .then(r => r.json())
+      .then(ingredients => this.setState({ingredients}))
+    fetch('https://react-boozer-backend.herokuapp.com/api/v1/proportions')
+      .then(r => r.json())
+      .then(proportions => this.setState({proportions}))
+  }
 
   render(){
     return (
       <div className="container">
-      
+        <CocktailsList
+          cocktails={this.state.cocktails}
+          showCocktail={this.showCocktail}  />
+        {this.state.showCocktail !== '' ? <CocktailDisplay
+          cocktail={this.state.showCocktail}
+          proportions={this.state.showProportions}
+          ingredients={this.state.showProportions.map(prop => this.findIngredient(prop.ingredient_id))}
+          /> : null}
+        <Form />
       </div>
     )
   }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,53 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+
+const IngredientInput = (props) => {
+  return (
+    <div className="container">
+      <p>Ingredient Name<br/>
+      <input type="text"/>
+      </p>
+
+      <p>Quantity<br/>
+      <input type="text"/>
+      </p>
+    </div>
+  )
+}
 
 class Form extends Component {
+  constructor() {
+    super()
+    this.state = {
+      numInputs: 1
+    }
+  }
+
+  addIngredientInput = (e) => {
+    e.preventDefault()
+    this.setState({numInputs: this.state.numInputs + 1})
+  }
+
+  allIngredientInputs = () => {
+    let inputArray = []
+    for (let i = 0; i < this.state.numInputs; i++) {
+      inputArray.push(<IngredientInput />)
+    }
+    return inputArray
+  }
+
+  createCocktail = (e) => {
+    e.preventDefault()
+    e.persist()
+    let inputs = e.target.querySelectorAll('input')
+  }
+
+  updateName = (e) => {
+    e.persist()
+  }
+
   render(){
     return (
-      <form>
+      <form onSubmit={this.createCocktail}>
         <h3>Create a Cocktail</h3>
 
         <p>Name</p>
@@ -16,17 +60,9 @@ class Form extends Component {
         <input type="text"/>
 
         <h3>Proportions</h3>
-        <div className="container">
-          <p>Ingredient Name<br/>
-          <input type="text"/>
-          </p>
+        {this.allIngredientInputs()}
 
-          <p>Quantity<br/>
-          <input type="text"/>
-          </p>
-        </div>
-
-        <p><button> + </button></p>
+        <p><button onClick={this.addIngredientInput}> + </button></p>
 
         <input type="submit"/>
       </form>
